21a86c14bde28cfef2b33a8e6b4e3e5c779051a3
Pham Duong <phamduong11@gmail.com>
2023-04-23 14:14:02 +0700

    Initial Ful-Stack-Open

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import { useState } from "react";
+import "./App.css";
+
+const App = () => {
+  // save clicks of each button to its own state
+
+  return <div className="container"></div>;
+};
+
+export default App;
