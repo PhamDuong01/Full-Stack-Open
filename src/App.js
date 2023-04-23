726f92086f3781691dae0665dfb496188488c881
Pham Duong <phamduong11@gmail.com>
2023-04-23 16:52:27 +0700

    1.12*: anecdotes step1

--- src/App.js
+++ src/App.js
@@ -1,76 +1,27 @@
 import { useState } from "react";
 import "./App.css";
 
-const StatisticsLine = (props) => {
-  return (
-    <tr>
-      <td>{props.text}</td>
-      <td>
-        {props.value}
-        {props.text === "positive" ? " %" : ""}
-      </td>
-    </tr>
-  );
-};
-
-const Statistics = (props) => {
-  const { good, neutral, bad } = props;
-  const all = good + neutral + bad;
-  const average = (good - bad) / all;
-  const positive = (good / all) * 100;
-
-  if (good + neutral + bad == 0) return <p>No feedback given</p>;
-  return (
-    <>
-      <h2>statistics</h2>
-      <table>
-        <tbody>
-          <StatisticsLine text="good" value={good} />
-          <StatisticsLine text="neutral" value={neutral} />
-          <StatisticsLine text="bad" value={bad} />
-          <StatisticsLine text="all" value={all} />
-          <StatisticsLine text="average" value={average} />
-          <StatisticsLine text="positive" value={positive} />
-        </tbody>
-      </table>
-    </>
-  );
-};
-
-const MyButton = (props) => {
-  const handleClick = () => {
-    props.onButtonClick();
-  };
-  return (
-    <>
-      <button onClick={handleClick}>{props.text}</button>
-    </>
-  );
-};
-
 const App = () => {
-  // save clicks of each button to its own state
-  const [good, setGood] = useState(0);
-  const [neutral, setNeutral] = useState(0);
-  const [bad, setBad] = useState(0);
+  const anecdotes = [
+    "If it hurts, do it more often.",
+    "Adding manpower to a late software project makes it later!",
+    "The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
+    "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
+    "Premature optimization is the root of all evil.",
+    "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
+    "Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.",
+    "The only way to go fast, is to go well.",
+  ];
 
-  function handleSetGood() {
-    setGood(good + 1);
-  }
-  function handleSetNeutral() {
-    setNeutral(neutral + 1);
-  }
-  function handleSetBad() {
-    setBad(bad + 1);
+  const [selected, setSelected] = useState(0);
+  function randomSelected() {
+    setSelected(Math.floor(Math.random() * anecdotes.length));
   }
 
   return (
-    <div className="container">
-      <h1>give feedback</h1>
-      <MyButton text="good" onButtonClick={handleSetGood} />
-      <MyButton text="neutral" onButtonClick={handleSetNeutral} />
-      <MyButton text="bad" onButtonClick={handleSetBad} />
-      <Statistics good={good} neutral={neutral} bad={bad} />
+    <div>
+      <p>{anecdotes[selected]}</p>
+      <button onClick={randomSelected}>next anecdote</button>
     </div>
   );
 };
