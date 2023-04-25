9c12503236914a3e31b0daa076050bb6d10cae38
Pham Duong <phamduong11@gmail.com>
2023-04-25 15:38:02 +0700

    2.4: Course information step9

--- src/Course.js
+++ src/Course.js
@@ -1,21 +1,31 @@
 const Course = (props) => {
-  const course = props.course;
-  const total = course.parts.reduce((s, p) => {
-    return s + p.exercises;
-  }, 0);
+  const courses = props.courses;
+
   return (
     <div>
-      <h1>{course.name}</h1>
-      {course.parts.map((part) => {
+      <h1>Web development curriculum</h1>
+      {courses.map((course) => {
+        const mycourse = course.parts.map((part) => {
+          return (
+            <p key={part.id}>
+              {part.name} {part.exercises}
+            </p>
+          );
+        });
+        const total = course.parts.reduce((s, p) => {
+          return s + p.exercises;
+        }, 0);
+
         return (
-          <p key={part.id}>
-            {part.name} {part.exercises}
-          </p>
+          <div key={course.id}>
+            <h2>{course.name}</h2>
+            {mycourse}
+            <p>
+              <strong>total of {total} exercises</strong>
+            </p>
+          </div>
         );
       })}
-      <p>
-        <strong> total of {total} exercises</strong>
-      </p>
     </div>
   );
 };
