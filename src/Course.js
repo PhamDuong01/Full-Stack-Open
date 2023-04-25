97d912fb8894fe552a074daaa90898448a2deec2
Pham Duong <phamduong11@gmail.com>
2023-04-25 15:14:23 +0700

    2.2: Course information step7

--- src/Course.js
+++ src/Course.js
@@ -1,15 +1,20 @@
 const Course = (props) => {
   const course = props.course;
+  let total = 0;
   return (
     <div>
       <h1>{course.name}</h1>
       {course.parts.map((part) => {
+        total += part.exercises;
         return (
           <p key={part.id}>
             {part.name} {part.exercises}
           </p>
         );
       })}
+      <p>
+        <strong> total of {total} exercises</strong>
+      </p>
     </div>
   );
 };
